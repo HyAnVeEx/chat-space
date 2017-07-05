a2c4ba1c0e3c04a16bf3e0cee9f4081d3b72ac3a
Your Name <you@example.com>
2017-07-05 17:40:37 +0900

    usersコントローラのupdateアクションを追記
    ●binding.pryを使用して、原因切り分けのためpry-railsのgemを導入
    ●updateを実行することにより、ユーザ情報を更新できるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,12 @@ class UsersController < ApplicationController
   def edit
   end
   def update
+    @user = User.find(params[:id])
+    @user.update(name: user_params[:name],email: user_params[:email])
+    redirect_to "/"
   end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
