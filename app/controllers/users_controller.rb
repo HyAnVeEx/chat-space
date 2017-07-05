1f7c71dffc2ec87d67bac3d846174cf0f0af1f2b
Your Name <you@example.com>
2017-07-05 19:05:54 +0900

    validates_presenceの設定/コントローラ編集
    validates_presenceをuser.rbに追記
    コントローラに条件分岐を追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,14 @@ class UsersController < ApplicationController
   end
   def update
     @user = User.find(params[:id])
+    if
     @user.update(name: user_params[:name],email: user_params[:email])
-    redirect_to "/"
+    redirect_to controller: 'messages', action: 'index'
+    else
+    render "edit"
+    end
   end
+
   private
     def user_params
       params.require(:user).permit(:name, :email)
