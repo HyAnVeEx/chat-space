994cbcc310726d7d2d269c045aa20221738a1bf9
Your Name <you@example.com>
2017-07-07 16:30:33 +0900

    グループ作成機能の実装
    グループ作成機能
    グループ作成時にメンバー追加
    登録時にフラッシュメッセージを表示
    登録失敗時にエラーメッセージ表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,27 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def create
+     @group = Group.new(group_params)
+    if @group.save
+      params[:group][:user_ids].each do |id|
+        @group.groups_users.create(user_id: id)
+      end
+      flash[:notice] = "グループを作成しました。"
+      redirect_to :root
+   else
+      render "new"
+    end
   end
 
   def edit
   end
 
+  private
+
+  def group_params
+     params.require(:group).permit(:name, :user_ids)
+  end
 end
