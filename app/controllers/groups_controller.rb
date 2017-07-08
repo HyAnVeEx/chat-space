1dcd5251ca003e6ff93644856008201bace2f450
Your Name <you@example.com>
2017-07-08 17:20:31 +0900

    コメント箇所の修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+    before_action :group_edit_update, only:[:edit,:update]
+
   def new
     @group = Group.new
   end
@@ -6,28 +8,29 @@ class GroupsController < ApplicationController
   def create
      @group = Group.new(group_params)
     if @group.save
-      redirect_to :root, notice: "グループを作成しました。"
+      redirect_to :root, notice: 'グループを作成しました。'
    else
-      render "new"
+      render :new
     end
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to :root, notice: "グループを編集しました。"
+      redirect_to :root, notice: 'グループを編集しました。'
     else
-      render "edit"
+      render :edit
     end
   end
 
   private
+    def group_params
+       params.require(:group).permit(:name, :user_ids => [])
+    end
+    def group_edit_update
+       @group = Group.find(params[:id])
+    end
 
-  def group_params
-     params.require(:group).permit(:name, :user_ids => [])
-  end
 end
