5c77ea14006a19a92cd64336bc6959931d45a241
Your Name <you@example.com>
2017-07-11 21:22:10 +0900

    ルーティング、コントローラー追加
    メッセージ送信、データ保存の確認まで実施

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @message = Message.new
   end
+
+  def create
+      @message = Message.new(content: message_params[:content], image: message_params[:image], user_id: current_user.id, group_id: params[:group_id])
+      @message.save
+  end
+
+    def message_params
+      # binding.pry
+       params.require(:message).permit(:content, :image)
+    end
+
 end
