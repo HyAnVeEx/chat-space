717b23735f7827e5d6b2277d8cdd4307bc3889ed
Your Name <you@example.com>
2017-07-12 14:15:08 +0900

    コメント箇所の修正・追記

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,29 @@
 class MessagesController < ApplicationController
+    before_action :set_group, only:[:index, :create]
+
   def index
-    @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @message = Message.new
   end
 
   def create
-    @message = Message.new(message_params)
-    @message.save
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to :group_messages, notice: 'メッセージを投稿しました。'
+    else
+    @groups = current_user.groups
+      render :index
+    end
+
   end
 
   private
   def message_params
-     params.require(:message).permit(:content, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+       @group = Group.find(params[:group_id])
   end
 
 end
