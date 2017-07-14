eed80871408f96742c3cdf236d9b110b1e53f659
Your Name <you@example.com>
2017-07-14 18:59:36 +0900

    モデルのテストに必要な機能を実装

---
+++ spec/factories/group.rb
@@ -0,0 +1,6 @@
+ FactoryGirl.define do
+  factory :group do
+    id 1
+    name Faker::Internet.name
+  end
+end
---
+++ spec/models/message_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe Message do
+  describe '#create' do
+    it "is valid with a content" do
+      message = build(:message, image:nil)
+      expect(message).to be_valid
+    end
+
+    it "is valid with a image" do
+      message = build(:message, content:nil)
+      expect(message).to be_valid
+    end
+
+    it "is valid with a content, image" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    it "is invalid without a content, image" do
+      message = build(:message, content: nil, image: nil,)
+      message.valid?
+      expect(message).not_to be_valid
+    end
+
+    it "is invalid without a group_id" do
+      message = build(:message, group_id: nil)
+      message.valid?
+      expect(message).not_to be_valid
+    end
+
+    it "is invalid without a user_id" do
+      message = build(:message, user_id: nil)
+      message.valid?
+      expect(message).not_to be_valid
+    end
+  end
+end
