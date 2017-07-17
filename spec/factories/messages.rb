b58b19a78fc6b3db7191346c6cdcb5debb30fb6b
Your Name <you@example.com>
2017-07-17 18:03:50 +0900

    factoryの設定追加

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,11 +1,6 @@
 FactoryGirl.define do
-
-
   factory :message do
-    id Faker::Number.digit
     content              Faker::Zelda.character
     image                Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.jpg'))
-    user_id              1
-    group_id             1
   end
 end
